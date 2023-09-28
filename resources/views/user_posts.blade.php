2cfd5381298dd5da782bdc4ea76b0ea33f08d265
nazaninns <nazaninnsq@gmail.com>
2023-09-28 17:12:31 +0330

    edit front user post

--- resources/views/user_posts.blade.php
+++ resources/views/user_posts.blade.php
@@ -1,6 +1,23 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite('resources/css/app.css')
+</head>
+<body class=" container mx-auto mt-20 bg-blue-400">
+
 @foreach($posts as $post)
-    <h1>{{$post->title}}</h1>
+    <div class="flex flex-col bg-blue-300 rounded-xl  gap-4 mt-14 p-2.5">
+    <h1 class="text-2xl text-gray-100">{{$post->title}}</h1>
 
-    <p>{{$post->content}}</p>
-
+    <p class="text-gray-600">{{$post->content}}</p>
+    </div>
 @endforeach
+
+</body>
+</html>
